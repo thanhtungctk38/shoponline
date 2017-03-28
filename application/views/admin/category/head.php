259081eeddc09a3e49ec0a43d450a3293e97bf21
Tùng núi <thanhtungctk38@gmail.com>
2017-03-28 16:27:11 +0700

    28/3

---
+++ application/views/admin/category/head.php
@@ -0,0 +1,24 @@
+<div class="titleArea">
+    <div class="wrapper">
+        <div class="pageTitle">
+            <h5>Danh mục </h5>
+            <span>Quản lý danh mục sản phẩm</span>
+        </div>
+
+        <div class="horControlB menu_action">
+            <ul>
+                <li><a href="admin/cat/add.html">
+                        <img src="public/admin/images/icons/control/16/add.png">
+                        <span>Thêm mới</span>
+                    </a></li>
+
+                <li><a href="admin/cat.html">
+                        <img src="public/admin/images/icons/control/16/list.png">
+                        <span>Danh sách</span>
+                    </a></li>
+            </ul>
+        </div>
+
+        <div class="clear"></div>
+    </div>
+</div>
