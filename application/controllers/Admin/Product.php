8c76b154e4b391aa8035cea3681edad0bea22e0a
Tùng núi <thanhtungctk38@gmail.com>
2017-03-30 10:14:12 +0700

    30/3

--- application/controllers/Admin/Product.php
+++ application/controllers/Admin/Product.php
@@ -10,6 +10,7 @@ class Product extends MY_Controller {
     function index() {
         //Load thư viên phân trang
         $this->load->library('pagination');
+        $this->load->model('category_model');
         $total = $this->product_model->get_total();
         $config = array(
             'total_rows' => $total,
@@ -18,25 +19,39 @@ class Product extends MY_Controller {
             'uri_segment' => 4,
             'next_link' => '>>',
             'prev_link' => '<<',
-             'first_link'=>'Đầu',
-            'last_link'=>'Cuối'
+            'first_link' => 'Đầu',
+            'last_link' => 'Cuối'
         );
         $this->pagination->initialize($config);
-        
-        $segment=$this->uri->segment(4);
-        $segment= intval($segment);
+
+        $segment = $this->uri->segment(4);
+        $segment = intval($segment);
         $input = array(
             'limit' => array($config['per_page'], $segment)
         );
-        
+
         $this->data = array(
             'message' => $this->session->flashdata('message'),
             'temp' => 'admin/product/index',
             'total' => $total,
-            'list' => $this->product_model->get_list($input)
+            'list' => $this->product_model->get_list($input),
+            'categories' => $this->category_model->get_list()
         );
 
         $this->load->view('admin/shared/layout', $this->data);
+    }
+
+    function delete() {
+        $id = $this->uri->segment(4);
+        $id = intval($id);
+        $info = $this->product_model->get_info($id);
+        if (!$info) {
+            $this->session->set_flashdata('message', 'Không tồn tại sản phẩm này');
+            redirect(admin_url('product'));
+        }
+        $this->product_model->delete($id);
+        $this->session->set_flashdata('message', 'Xóa sản phẩm thành công');
+        redirect(admin_url('product'));
     }
 
 }
